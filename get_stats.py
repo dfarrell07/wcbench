6c733cff2ff54d21c1280930873b99165990443c
Daniel Farrell <dfarrell07@gmail.com>
2014-06-16 14:49:39 -0400

    Compute used_RAM stats

--- get_stats.py
+++ get_stats.py
@@ -9,6 +9,7 @@ results_file = "results.csv"
 
 flows_col = []
 runtime_col = []
+used_ram_col = []
 with open(results_file, "rb") as f:
     reader = csv.reader(f)
     for row in reader:
@@ -16,7 +17,7 @@ with open(results_file, "rb") as f:
             flows_col.append(float(row[1]))
             # Subtract end_time from start_time to get CBench runtime
             runtime_col.append(float(row[3]) - float(row[2]))
-            print float(row[4]) - float(row[3])
+            used_ram_col.append(float(row[11]))
         except ValueError:
             # Skips header
             continue
@@ -35,4 +36,11 @@ results["runtime_min"] = int(round(numpy.amin(runtime_col)))
 results["runtime_max"] = int(round(numpy.amax(runtime_col)))
 results["runtime_mean"] = int(round(numpy.mean(runtime_col)))
 results["runtime_standard_deviation"] = int(round(numpy.std(runtime_col)))
+
+# Calculate used RAM stats
+results["used_ram_min"] = int(round(numpy.amin(used_ram_col)))
+results["used_ram_max"] = int(round(numpy.amax(used_ram_col)))
+results["used_ram_mean"] = int(round(numpy.mean(used_ram_col)))
+results["used_ram_standard_deviation"] = int(round(numpy.std(used_ram_col)))
+
 pprint.pprint(results)
