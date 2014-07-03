f21f43c5af2140b0b1b31a032ef9da78719a0fb7
Daniel Farrell <dfarrell07@gmail.com>
2014-07-03 17:03:07 -0400

    Simplified cycle-burning script

--- burn_cycles.py
+++ burn_cycles.py
@@ -4,12 +4,9 @@ import threading
 
 def cycle_burner():
     while True:
-        blah = 9912313 * 34023840293
         meh = 84908230489 % 323422
 
-threads = []
 for i in range(3):
     thread = threading.Thread(target=cycle_burner)
-    threads.append(thread)
     print "Starting a thread"
     thread.start()
