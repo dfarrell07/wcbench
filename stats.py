23598c13dc70a18e13c1f26abdeb3ae14abf5967
Daniel Farrell <dfarrell07@gmail.com>
2014-07-12 21:52:39 -0400

    Better handling of graph args to stats.py, relevant to #30

--- stats.py
+++ stats.py
@@ -8,24 +8,6 @@ import matplotlib.pyplot as pyplot
 import argparse
 import sys
 
-description = "Compute stats about CBench data"
-parser = argparse.ArgumentParser(description)
-parser.add_argument("-a", "--all", action="store_true",
-    help="compute all stats")
-parser.add_argument("-A", "--all-graphs", action="store_true",
-    help="compute all stats")
-parser.add_argument("-f", "--flow", action="store_true",
-    help="compute flows/sec stats")
-parser.add_argument("-F", "--flow-graph", action="store_true",
-    help="graph flows/sec data")
-
-# Print help if no arguments are given
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
-
-# Parse the given args
-args = parser.parse_args()
 
 class Stats(object):
 
@@ -36,12 +18,21 @@ class Stats(object):
     precision = 3
     run_index = 0
     flow_index = 1
+    start_time_index = 2
+    end_time_index = 3
+    start_steal_time_index = 10
+    end_steal_time_index = 11
+    used_ram_index = 13
+    one_load_index = 16
+    five_load_index = 17
+    fifteen_load_index = 18
+    start_iowait_index = 20
+    end_iowait_index = 21
 
     def __init__(self):
         """Setup some flags and data structures, kick off build_cols call."""
         self.build_cols()
         self.results = {}
-        self.some_graph_built = False
         self.some_stats_computed = False
         self.results["sample_size"] = len(self.run_col)
 
@@ -49,56 +40,128 @@ class Stats(object):
         """Parse results file into lists of values, one per column."""
         self.run_col= []
         self.flows_col = []
+        self.runtime_col = []
+        self.used_ram_col = []
+        self.iowait_col = []
+        self.steal_time_col = []
+        self.one_load_col = []
+        self.five_load_col = []
+        self.fifteen_load_col = []
+
         with open(self.results_file, "rb") as results_fd:
             results_reader = csv.reader(results_fd)
             for row in results_reader:
                 try:
                     self.run_col.append(float(row[self.run_index]))
                     self.flows_col.append(float(row[self.flow_index]))
+                    self.runtime_col.append(float(row[self.end_time_index]) - \
+                        float(row[self.start_time_index]))
+                    self.used_ram_col.append(float(row[self.used_ram_index]))
+                    self.iowait_col.append(float(row[self.end_iowait_index]) - \
+                        float(row[self.start_iowait_index]))
+                    self.steal_time_col.append(float(row[self.end_steal_time_index]) - \
+                        float(row[self.start_steal_time_index]))
+                    self.one_load_col.append(float(row[self.one_load_index]))
+                    self.five_load_col.append(float(row[self.five_load_index]))
+                    self.fifteen_load_col.append(float(row[self.fifteen_load_index]))
                 except ValueError:
                     # Skips header
                     continue
 
     def compute_flow_stats(self):
-        """Calculate CBench flows/second stats."""
+        """Compute CBench flows/second stats."""
         flow_stats = {}
         flow_stats["min"] = int(round(numpy.amin(self.flows_col)))
         flow_stats["max"] = int(round(numpy.amax(self.flows_col)))
         flow_stats["mean"] = round(numpy.mean(self.flows_col),
                                                        self.precision)
         flow_stats["stddev"] = round(numpy.std(self.flows_col), self.precision)
-        flow_stats["relstddev"] = round((numpy.std(self.flows_col) / numpy.mean(self.flows_col)) * 100, self.precision)
+        flow_stats["relstddev"] = round((numpy.std(self.flows_col) / \
+            numpy.mean(self.flows_col)) * \
+            100, self.precision)
         self.results["flows"] = flow_stats
         self.some_stats_computed = True
 
-    def build_flow_graph(self):
+    def build_flow_graph(self, total_graph_count, graph_num):
         """Plot flows/sec data."""
-        #pyplot.subplot(2, 1, 1)
-        # "go" means green O's. pyplot syntax is -"Explicit is better than implicit"
+        pyplot.subplot(total_graph_count, 1, graph_num)
+        # "go" means green O's
         pyplot.plot(self.run_col, self.flows_col, "go")
         pyplot.xlabel("Run Number")
         pyplot.ylabel("Flows per Second")
-        self.some_graph_built = True
+
+    def compute_ram_stats(self):
+        """Compute used RAM stats."""
+        ram_stats = {}
+        ram_stats["min"] = int(numpy.amin(self.used_ram_col))
+        ram_stats["max"] = int(numpy.amax(self.used_ram_col))
+        ram_stats["mean"] = round(numpy.mean(self.used_ram_col), self.precision)
+        ram_stats["stddev"] = round(numpy.std(self.used_ram_col), self.precision)
+        self.results["ram"] = ram_stats
+        self.some_stats_computed = True
+
+    def build_ram_graph(self, total_graph_count, graph_num):
+        """Plot used RAM data."""
+        # Params are numrows, numcols, fignum
+        pyplot.subplot(total_graph_count, 1, graph_num)
+        # "go" means green O's
+        pyplot.plot(self.run_col, self.used_ram_col, "go")
+        pyplot.xlabel("Run Number")
+        pyplot.ylabel("Used RAM")
+
 
 stats = Stats()
 
-if args.all:
-    stats.compute_flow_stats()
+# Map of graph names to the Stats.fns that build them
+graph_map = {"flows": stats.build_flow_graph,
+             "ram": stats.build_ram_graph}
+
+parser = argparse.ArgumentParser(description="Compute stats about CBench data")
+parser.add_argument("-a", "--all", action="store_true",
+    help="compute all stats")
+parser.add_argument("-A", "--all-graphs", action="store_true",
+    help="graph all data")
+parser.add_argument("-g", "--graphs", choices=graph_map.keys(),
+    help="graph given data", nargs="+")
+parser.add_argument("-f", "--flows", action="store_true",
+    help="compute flows/sec stats")
+parser.add_argument("-r", "--ram", action="store_true",
+    help="compute used RAM stats")
+
+# Print help if no arguments are given
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+# Parse the given args
+args = parser.parse_args()
 
 if args.all_graphs:
-    stats.build_flow_graph()
+    for graph_fn in graph_map.values():
+        graph_fn()
+else:
+    for graph, graph_num in zip(args.graphs, range(len(args.graphs))):
+        graph_map[graph](len(args.graphs), graph_num+1)
 
-if args.flow:
+if args.flows or args.all:
     stats.compute_flow_stats()
 
-if args.flow_graph:
-    stats.build_flow_graph()
+if args.ram or args.all:
+    stats.compute_ram_stats()
 
 if stats.some_stats_computed:
     # Report stat results
     pprint.pprint(stats.results)
 
-if stats.some_graph_built:
+if args.graphs or args.all_graphs:
     # Render plot
-    pyplot.subplots_adjust(hspace=.3)
+    if len(args.graphs) <= 3:
+        pyplot.subplots_adjust(hspace=.2)
+    elif len(args.graphs) <= 6:
+        pyplot.subplots_adjust(hspace=.4)
+    elif len(args.graphs) <= 9:
+        pyplot.subplots_adjust(hspace=.7)
+    else:
+        pyplot.subplots_adjust(hspace=.7)
+        print "WARNING: That's a lot of graphs. Add a second column?"
     pyplot.show()
